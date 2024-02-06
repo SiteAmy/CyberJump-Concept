735ae5a3166c758afab49a063bcc35c4ef12800c
SiteAmy <81807368+SiteAmy@users.noreply.github.com>
2024-02-06 09:55:56 +0100

    Concept project

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int points = 0;
+
+    [SerializeField] private Text pointsText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Collectable"))
+        {
+            Destroy(collision.gameObject);
+            points++;
+            pointsText.text = "Points: " + points;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerLife : MonoBehaviour
+{
+
+    private static int deaths = 0;
+    [SerializeField] private Text deathText;
+
+
+    private void DontDestroyOnLoad(int deaths)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            
+            deaths++;
+            deathText.text = "Deaths: " + deaths;
+            Die();
+        }
+
+        else if (collision.gameObject.CompareTag("Win"))
+        {
+            Win();
+        }
+    }
+
+    private void Die()
+    {
+        RestartLevel();
+    }
+
+    private void Win()
+    {
+        RestartLevel();
+        Debug.Log("Winner");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void RestartButton()
+    {
+        RestartLevel();
+    }
+
+
+}
